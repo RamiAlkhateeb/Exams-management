11830fc16f17acc625862e753c8b4a5906255970
mhdob <mhd.obeid94@gmail.com>
2019-03-31 16:03:28 +0300

    MHDOB

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminDashBoardComponent } from './admin-dash-board/admin-dash-board.component';
+import { Category } from './category/category';
+import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
-  { path : 'admindashboard' , component : AdminDashBoardComponent }
+  {
+    path: 'admindashboard', component: AdminDashBoardComponent,
+    children: [
+      { path: 'AddCategory', component: CategoryComponent ,outlet : "User"},
+      { path: 'AddUser', component: CategoryComponent },
+      { path: 'AddQuastion', component: CategoryComponent },
+      { path: 'AddExam', component: CategoryComponent },
+      { path: 'ManageExam', component: CategoryComponent }
+    ]
+  }
 ];
 
 @NgModule({
